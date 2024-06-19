2665866dc955ad0e800ceb6ea993b218a29cb8f1
luizcardosodev <luizcardosodev@gmail.com>
2024-06-19 14:40:56 -0300

    added angular material components.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserTableComponent } from './components/user-table/user-table.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UserTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
